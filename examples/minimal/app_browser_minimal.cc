85f06df4e1961a0e2390d09aa23ea79b8ec2ee84
Diego Pino Garcia <dpino@igalia.com>
2023-11-17 10:01:30 +0100

    Honor '--url' command line argument

--- examples/minimal/app_browser_minimal.cc
+++ examples/minimal/app_browser_minimal.cc
@@ -39,8 +39,14 @@ class BrowserApp : public CefApp, public CefBrowserProcessHandler {
 
   // CefBrowserProcessHandler methods:
   void OnContextInitialized() override {
+    CefRefPtr<CefCommandLine> command_line = CefCommandLine::GetGlobalCommandLine();
+
+    std::string url = command_line->GetSwitchValue("url");
+    if (url.empty()) {
+      url = kStartupURL;
+    }
     // Create the browser window.
-    shared::CreateBrowser(new Client(), kStartupURL, CefBrowserSettings());
+    shared::CreateBrowser(new Client(), url, CefBrowserSettings());
   }
 
  private:
